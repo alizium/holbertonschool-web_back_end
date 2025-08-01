566b7e42946a1ee3fbd9ca959a64d9e9986e22d0
alizium <10549@holbertonstudents.com>
2025-08-01 11:47:25 +0200

    MAINTENANT C'EST PYCODE LE PB

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -2,6 +2,8 @@
 
 # 0-add.py
 """Retourne la somme de a et b en tant que float."""
+
+
 def add(a: float, b: float) -> float:
     """Retourne la somme de a et b en tant que float."""
     return a + b
