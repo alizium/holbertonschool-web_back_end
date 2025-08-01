2b61e4ccc710e9fba6f5ff181a4e6545e491eebf
alizium <10549@holbertonstudents.com>
2025-08-01 11:46:47 +0200

    CHANGEMENT

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 # 0-add.py
+"""Retourne la somme de a et b en tant que float."""
 def add(a: float, b: float) -> float:
     """Retourne la somme de a et b en tant que float."""
     return a + b
