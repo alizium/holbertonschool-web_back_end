bb40ec0eeb4d5372af8c9eef8266229b117366f3
alizium <10549@holbertonstudents.com>
2025-08-03 22:37:28 +0200

    correction

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -2,9 +2,8 @@
 
 """Async Comprehensions."""
 
-from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
-async def async_comprehension() -> List[float]:
+async def async_comprehension():
     """Collect 10 numbers from async_generator and return them as a list."""
-    return [i async for i in async_generator()]
+    return [num async for num in async_generator()]
