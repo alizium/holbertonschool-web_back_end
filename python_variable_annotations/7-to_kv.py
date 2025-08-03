07198fb2cbbaa2df75ae4d1ef9fb2c30b207084f
alizium <10549@holbertonstudents.com>
2025-08-03 22:49:00 +0200

    correction

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,10 +1,28 @@
 #!/usr/bin/env python3
 
-"""Complex types - string and int/float to tuple."""
+"""
+Module to define a function that takes a key and a value,
+calculates the square of the value, and returns a tuple of the key
+and the squared value.
 
-from typing import Union, Tuple
+This function ensures that the squared value is returned as a float.
+"""
+
+from typing import Tuple, Union
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """Returns a tuple with a string and the square of an int or float as float."""
-    return (k, float(v ** 2))
+    """
+    Takes a key (k) and a value (v), squares the value, and returns a tuple
+    containing the key and the squared value as a float.
+
+    Args:
+        k (str): The key to be paired with the squared value.
+        v (Union[int, float]): The value to be squared, which can be an integer
+                                or a float.
+
+    Returns:
+        Tuple[str, float]: A tuple containing the key and the squared value
+                            as a float.
+    """
+    return k, float(v**2)
