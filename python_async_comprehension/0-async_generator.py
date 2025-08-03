9aaecd13b4115bd734ea24fea571dda0577d60c2
alizium <10549@holbertonstudents.com>
2025-08-03 22:34:47 +0200

    correction

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,9 +4,8 @@
 
 import asyncio
 import random
-from typing import AsyncGenerator
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator():
     """Yield a random float between 0 and 10, 10 times, with a 1s delay each."""
     for _ in range(10):
         await asyncio.sleep(1)
