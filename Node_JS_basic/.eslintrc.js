f774523a92356e3d34f7dc449d7a46ee9bf2fcc9
alizium <10549@holbertonstudents.com>
2025-09-04 10:48:14 +0200

    .

--- Node_JS_basic/.eslintrc.js
+++ Node_JS_basic/.eslintrc.js
@@ -1,22 +1,37 @@
 module.exports = {
   env: {
     browser: false,
-    es2021: true,
-    node: true,
+    es6: true,
     jest: true,
   },
-  extends: ['airbnb-base'],
+  extends: [
+    'airbnb-base',
+    'plugin:jest/all',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
   parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'script',
+    ecmaVersion: 2018,
+    sourceType: 'module',
   },
+  plugins: ['jest'],
   rules: {
-    'no-console': 'off',
-    'func-names': 'off',
-    'no-param-reassign': 'off',
+    'max-classes-per-file': 'off',
     'no-underscore-dangle': 'off',
-    'no-restricted-syntax': 'off',
-    'import/extensions': 'off',
-    'comma-dangle': ['error', 'always-multiline'],
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
   },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
 };
